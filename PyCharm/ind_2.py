9cb5390ee1954bebff16c8fd76e903ce21c0efad
KuvshinChick <kuvshinirina75@gmail.com>
2022-10-29 06:53:24 +0300

    add ind_3

--- PyCharm/ind_2.py
+++ PyCharm/ind_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# import complex math module
+# import math module
 import math
 
 if __name__ == "__main__":
---
+++ PyCharm/ind_3.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == "__main__":
+    # Input variables
+    n = int(input("Введите разряд чисел n: "))
+    k = int(input("Введите делитель k: "))
+    i = (10 ** n) - 1
+    s = 0
+
+    # Divisibility check cycle
+    while (i >= (10 ** (n-1))):
+        if(i % k == 0):
+            s += i
+        i -= 1
+    print("Сумма всех чисел кратных k:")
+    print(s)
